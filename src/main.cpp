57d06d3dd0534dd0546cfeb22ab9bda32628b25c
Mohamed Abbas <mohamed71992@gmail.com>
2020-05-28 21:56:46 +0200

    Take input coordinates from the user

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <string>
 #include <io2d.h>
+#include<ios>
+#include<limits> 
 #include "route_model.h"
 #include "render.h"
 #include "route_planner.h"
@@ -11,14 +13,14 @@
 using namespace std::experimental;
 
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
-{   
+{
     std::ifstream is{path, std::ios::binary | std::ios::ate};
     if( !is )
         return std::nullopt;
-    
+
     auto size = is.tellg();
-    std::vector<std::byte> contents(size);    
-    
+    std::vector<std::byte> contents(size);
+
     is.seekg(0);
     is.read((char*)contents.data(), size);
 
@@ -27,8 +29,23 @@ static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
     return std::move(contents);
 }
 
+// Read coordinate input from the user and validate its value
+static float ReadInputCoordinate()
+{
+    float coordinate;
+
+    std::cin >> coordinate;
+    while (!(std::cin) || coordinate < 0 || coordinate > 100) {
+        std::cout << "Invalid entry. Please Enter value from 0 to 100: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> coordinate;
+    }
+    return coordinate;
+}
+
 int main(int argc, const char **argv)
-{    
+{
     std::string osm_data_file = "";
     if( argc > 1 ) {
         for( int i = 1; i < argc; ++i )
@@ -40,9 +57,9 @@ int main(int argc, const char **argv)
         std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
         osm_data_file = "../map.osm";
     }
-    
+
     std::vector<std::byte> osm_data;
- 
+
     if( osm_data.empty() && !osm_data_file.empty() ) {
         std::cout << "Reading OpenStreetMap data from the following file: " <<  osm_data_file << std::endl;
         auto data = ReadFile(osm_data_file);
@@ -51,16 +68,27 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+
+    // Read the input from the user
+    float start_x, start_y, end_x, end_y;
+
+    std::cout << "Enter a start_x from 0 to 100: ";
+    start_x = ReadInputCoordinate();
+
+    std::cout << "Enter a start_y from 0 to 100: ";
+    start_y = ReadInputCoordinate();
+
+    std::cout << "Enter a end_x from 0 to 100: ";
+    end_x = ReadInputCoordinate();
+
+    std::cout << "Enter a end_y from 0 to 100: ";
+    end_y = ReadInputCoordinate();
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
